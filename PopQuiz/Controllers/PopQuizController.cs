4be0774f78cbb252e1ef625bed13893c6b48682a
Peter Slavin <pbslavin@gmail.com>
2018-08-02 18:49:54 -0500

    Pop Quiz

---
+++ PopQuiz/Controllers/PopQuizController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PopQuiz.Controllers
+{
+    public class PopQuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
